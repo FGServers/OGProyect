5187276f4b656d07115b7d0f6dc4671d135aa732
FGServers <jonatanvilella@gmail.com>
2015-04-29 19:05:38 -0300

    Sistema de posición
    Nuevo sistema de posición de los planetas e imagenes cambiadas al del
    rediseño

---
+++ application/config/config.php
@@ -0,0 +1,9 @@
+<?php
+if(!defined("INSIDE")){ header("location:./../"); }
+defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
+defined('DB_USER') ? NULL : define('DB_USER', 'root');
+defined('DB_PASS') ? NULL : define('DB_PASS', 'root');
+defined('DB_NAME') ? NULL : define('DB_NAME', 'ogp');
+defined('DB_PREFIX') ? NULL : define('DB_PREFIX', 'ogp_');
+defined('SECRETWORD') ? NULL : define('SECRETWORD', 'ogp-1RgtYYmAN8hadds6');
+?>
